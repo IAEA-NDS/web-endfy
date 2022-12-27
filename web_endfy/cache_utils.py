a207e7b6d9088bcd6c2e817d31dd041cb15e071f
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-12-27 13:57:25 +0100

    add use_pickle option to cache access functions

--- web_endfy/cache_utils.py
+++ web_endfy/cache_utils.py
@@ -1,30 +1,38 @@
 import os
 import tempfile
 import logging
+import pickle
 
 
-def fetch_file_from_cachedir(cachedir, fname, mode='binary'):
+def fetch_file_from_cachedir(cachedir, fname, mode='binary', use_pickle=False):
     if mode not in ('text', 'binary'):
         raise ValueError('mode arg must be either "text" or "binary"')
     if cachedir is None:
         return None
+    if use_pickle:
+        mode = 'binary'
     os.makedirs(cachedir, exist_ok=True)
     fp = os.path.join(cachedir, fname)
     if os.path.exists(fp):
         logging.info(f'retrieve content of file {fname} from cache')
         fmode = 'rb' if mode == 'binary' else 'rt'
         with open(fp, fmode) as fr:
-            cont = fr.read()
+            if use_pickle:
+                cont = pickle.load(fr)
+            else:
+                cont = fr.read()
         return cont
     else:
         return None
 
 
-def store_file_in_cachedir(cachedir, fname, cont, mode='binary'):
+def store_file_in_cachedir(cachedir, fname, cont, mode='binary', use_pickle=False):
     if mode not in ('text', 'binary'):
         raise ValueError('mode arg must be either "text" or "binary"')
     if cachedir is None:
         return
+    if use_pickle:
+        mode = 'binary'
     fp = os.path.join(cachedir, fname)
     # jump through some hoops to guarantee atomicity
     # of the file creation process
@@ -34,7 +42,10 @@ def store_file_in_cachedir(cachedir, fname, cont, mode='binary'):
         temp_file = tempfile.NamedTemporaryFile(
             delete=False, dir=cachedir, mode=fmode
         )
-        temp_file.write(cont)
+        if use_pickle:
+            pickle.dump(cont, temp_file)
+        else:
+            temp_file.write(cont)
         temp_file.close()
         temp_filepath = temp_file.name
         os.rename(temp_filepath, fp)
