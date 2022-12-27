16ba762bc6d8795a62b02ebd95dd2a4fc4414589
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-12-27 19:19:26 +0100

    refactor nds_archive_irdff.py

--- web_endfy/nds_archive_irdff.py
+++ web_endfy/nds_archive_irdff.py
@@ -1,36 +1,30 @@
 from .endf_archive_downloader import EndfArchiveDownloader
 
 
-def create_irdff1_library_list(cache_dir, trafo_cache_ext):
-    irdff_list = []
-    for sublib in ('n',):
-        irdff_list.append(
-            EndfArchiveDownloader(
-                liburl=f'IRDFF/{sublib}/',
-                libspec=f'irdff_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return irdff_list
-
-
-def create_irdff2_library_list(cache_dir, trafo_cache_ext):
-    irdff2_list = []
-    for sublib in ('n',):
-        irdff2_list.append(
-            EndfArchiveDownloader(
-                liburl=f'IRDFF-II/{sublib}/',
-                libspec=f'irdff2_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return irdff2_list
+irdff_libraries_dict = {
+    'irdff': {
+        'liburl': 'IRDFF',
+        'sublibs': ('n',)
+    },
+    'irdff2': {
+        'liburl': 'IRDFF-II',
+        'sublibs': ('n',)
+    }
+}
 
 
 def create_irdff_library_list(cache_dir=None, trafo_cache_ext=None):
     irdff_list = []
-    irdff_list.extend(create_irdff1_library_list(cache_dir, trafo_cache_ext))
-    irdff_list.extend(create_irdff2_library_list(cache_dir, trafo_cache_ext))
+    for curlibname, curlib in irdff_libraries_dict.items():
+        for cursublib in curlib['sublibs']:
+            liburl = curlib['liburl'] + '/' + cursublib + '/'
+            libspec = curlibname + '_' + cursublib
+            irdff_list.append(
+                EndfArchiveDownloader(
+                    liburl=liburl,
+                    libspec=libspec,
+                    cache_dir=cache_dir,
+                    trafo_cache_ext=trafo_cache_ext
+                )
+            )
     return irdff_list
