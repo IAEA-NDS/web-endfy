ba1f3c8bf63a19cc6fc0763f3c081b38582b2ddf
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-12-27 19:28:00 +0100

    refactor nds_archive_brond_rosfond.py

--- web_endfy/nds_archive_brond_rosfond.py
+++ web_endfy/nds_archive_brond_rosfond.py
@@ -1,51 +1,34 @@
 from .endf_archive_downloader import EndfArchiveDownloader
 
 
-def create_rosfond2010_library_list(cache_dir=None, trafo_cache_ext=None):
-    rosfond2010_list = []
-    for sublib in ('n',):
-        rosfond2010_list.append(
-            EndfArchiveDownloader(
-                liburl=f'ROSFOND-2010/{sublib}/',
-                libspec=f'rosfond2010_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return rosfond2010_list
-
-
-def create_brond22_library_list(cache_dir=None, trafo_cache_ext=None):
-    brond22_list = []
-    for sublib in ('n',):
-        brond22_list.append(
-            EndfArchiveDownloader(
-                liburl=f'BROND-2-2/{sublib}/',
-                libspec=f'brond22_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return brond22_list
-
-
-def create_brond31_library_list(cache_dir=None, trafo_cache_ext=None):
-    brond31_list = []
-    for sublib in ('n',):
-        brond31_list.append(
-            EndfArchiveDownloader(
-                liburl=f'BROND-3.1/{sublib}/',
-                libspec=f'brond31_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return brond31_list
+brond_rosfond_libraries_dict = {
+    'rosfond2010': {
+        'liburl': 'ROSFOND-2010',
+        'sublibs': ('n',)
+    },
+    'brond22': {
+        'liburl': 'BROND-2-2',
+        'sublibs': ('n',)
+    },
+    'brond31': {
+        'liburl': 'BROND-3.1',
+        'sublibs': ('n',)
+    }
+}
 
 
 def create_brond_rosfond_library_list(cache_dir=None, trafo_cache_ext=None):
-    brond_list = []
-    brond_list.extend(create_rosfond2010_library_list(cache_dir, trafo_cache_ext))
-    brond_list.extend(create_brond22_library_list(cache_dir, trafo_cache_ext))
-    brond_list.extend(create_brond31_library_list(cache_dir, trafo_cache_ext))
-    return brond_list
+    brond_rosfond_list = []
+    for curlibname, curlib in brond_rosfond_libraries_dict.items():
+        for cursublib in curlib['sublibs']:
+            liburl = curlib['liburl'] + '/' + cursublib + '/'
+            libspec = curlibname + '_' + cursublib
+            brond_rosfond_list.append(
+                EndfArchiveDownloader(
+                    liburl=liburl,
+                    libspec=libspec,
+                    cache_dir=cache_dir,
+                    trafo_cache_ext=trafo_cache_ext
+                )
+            )
+    return brond_rosfond_list
