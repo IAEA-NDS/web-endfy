6afec977ff53c2633c35982d1256b4767d354c85
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-12-27 19:15:02 +0100

    refactor nds_archive_fendl.py

--- web_endfy/nds_archive_fendl.py
+++ web_endfy/nds_archive_fendl.py
@@ -1,66 +1,38 @@
 from .endf_archive_downloader import EndfArchiveDownloader
 
 
-def create_fendl21_library_list(cache_dir, trafo_cache_ext):
-    fendl21_list = []
-    for sublib in ('n',):
-        fendl21_list.append(
-            EndfArchiveDownloader(
-                liburl=f'FENDL-2.1/{sublib}/',
-                libspec=f'fendl21_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return fendl21_list
-
-
-def create_fendl31c_library_list(cache_dir, trafo_cache_ext):
-    fendl31c_list = []
-    for sublib in ('n', 'p', 'd', 'photo'):
-        fendl31c_list.append(
-            EndfArchiveDownloader(
-                liburl=f'FENDL-3.1c/{sublib}/',
-                libspec=f'fendl31c_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return fendl31c_list
-
-
-def create_fendl32_library_list(cache_dir, trafo_cache_ext):
-    fendl32_list = []
-    for sublib in ('n', 'p', 'd', 'photo'):
-        fendl32_list.append(
-            EndfArchiveDownloader(
-                liburl=f'FENDL-3.2/{sublib}/',
-                libspec=f'fendl32_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return fendl32_list
-
-
-def create_fendl32b_library_list(cache_dir, trafo_cache_ext):
-    fendl32b_list = []
-    for sublib in ('n', 'p', 'd', 'photo'):
-        fendl32b_list.append(
-            EndfArchiveDownloader(
-                liburl=f'FENDL-3.2b/{sublib}/',
-                libspec=f'fendl32b_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return fendl32b_list
+fendl_libraries_dict = {
+    'fendl21': {
+        'liburl': 'FENDL-2.1',
+        'sublibs': ('n',)
+    },
+    'fendl31c': {
+        'liburl': 'FENDL-3.1c',
+        'sublibs': ('n', 'p', 'd', 'photo')
+    },
+    'fendl32': {
+        'liburl': 'FENDL-3.2',
+        'sublibs': ('n', 'p', 'd', 'photo')
+    },
+    'fendl32b': {
+        'liburl': 'FENDL-3.2b',
+        'sublibs': ('n', 'p', 'd', 'photo')
+    }
+}
 
 
 def create_fendl_library_list(cache_dir=None, trafo_cache_ext=None):
     fendl_list = []
-    fendl_list.extend(create_fendl21_library_list(cache_dir, trafo_cache_ext))
-    fendl_list.extend(create_fendl31c_library_list(cache_dir, trafo_cache_ext))
-    fendl_list.extend(create_fendl32_library_list(cache_dir, trafo_cache_ext))
-    fendl_list.extend(create_fendl32b_library_list(cache_dir, trafo_cache_ext))
+    for curlibname, curlib in fendl_libraries_dict.items():
+        for cursublib in curlib['sublibs']:
+            liburl = curlib['liburl'] + '/' + cursublib + '/'
+            libspec = curlibname + '_' + cursublib
+            fendl_list.append(
+                EndfArchiveDownloader(
+                    liburl=liburl,
+                    libspec=libspec,
+                    cache_dir=cache_dir,
+                    trafo_cache_ext=trafo_cache_ext
+                )
+            )
     return fendl_list
