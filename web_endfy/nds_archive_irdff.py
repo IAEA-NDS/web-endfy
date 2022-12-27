929b57c42bfe23fff55ef3d4c8ed3372b50a732d
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-12-27 17:29:56 +0100

    remove rex specification for irdff

--- web_endfy/nds_archive_irdff.py
+++ web_endfy/nds_archive_irdff.py
@@ -9,9 +9,7 @@ def create_irdff1_library_list(cache_dir, trafo_cache_ext):
                 liburl=f'IRDFF/{sublib}/',
                 libspec=f'irdff_{sublib}',
                 cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext,
-                rex='^(?P<projectile>[a-zA-Z0-9]+)_(?P<mat>[0-9]+)_(?P<charge>[0-9]+)-(?P<element>[a-zA-Z]+)-(?P<mass>[0-9]+)(?P<level>M?)',
-                dtypes={'projectile': str, 'charge': int, 'element': lambda x: x.title(), 'mass': int, 'mat': int, 'level': str}
+                trafo_cache_ext=trafo_cache_ext
             )
         )
     return irdff_list
@@ -25,9 +23,7 @@ def create_irdff2_library_list(cache_dir, trafo_cache_ext):
                 liburl=f'IRDFF-II/{sublib}/',
                 libspec=f'irdff2_{sublib}',
                 cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext,
-                rex='^(?P<projectile>[a-zA-Z0-9]+)_(?P<charge>[0-9]+)-(?P<element>[a-zA-Z]+)-(?P<mass>[0-9]+)(?P<level>M?)_(?P<mat>[0-9]+)',
-                dtypes={'projectile': str, 'charge': int, 'element': lambda x: x.title(), 'mass': int, 'mat': int, 'level': str}
+                trafo_cache_ext=trafo_cache_ext
             )
         )
     return irdff2_list
