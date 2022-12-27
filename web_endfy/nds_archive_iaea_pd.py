27ae4acb522339b12ac5a1b02b64d1a743543f3f
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-12-27 17:31:28 +0100

    remove rex specification for iaea-pd

--- web_endfy/nds_archive_iaea_pd.py
+++ web_endfy/nds_archive_iaea_pd.py
@@ -9,9 +9,7 @@ def create_iaea_pd1999_library_list(cache_dir, trafo_cache_ext):
                 liburl=f'IAEA-PD-1999/{sublib}/',
                 libspec=f'iaea_pd1999_{sublib}',
                 cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext,
-                rex='^(?P<projectile>[a-zA-Z0-9]+)_(?P<mat>[0-9]+)_(?P<charge>[0-9]+)-(?P<element>[a-zA-Z]+)-(?P<mass>[0-9]+)',
-                dtypes={'projectile': str, 'charge': int, 'element': lambda x: x.title(), 'mass': int, 'mat': int}
+                trafo_cache_ext=trafo_cache_ext
             )
         )
     return iaea_pd1999_list
@@ -25,9 +23,7 @@ def create_iaea_pd2019_library_list(cache_dir, trafo_cache_ext):
                 liburl=f'IAEA-PD-2019/{sublib}/',
                 libspec=f'iaea_pd2019_{sublib}',
                 cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext,
-                rex='^(?P<projectile>[a-zA-Z0-9]+)_(?P<charge>[0-9]+)-(?P<element>[a-zA-Z]+)-(?P<mass>[0-9]+)_(?P<mat>[0-9]+)',
-                dtypes={'projectile': str, 'charge': int, 'element': lambda x: x.title(), 'mass': int, 'mat': int}
+                trafo_cache_ext=trafo_cache_ext
             )
         )
     return iaea_pd2019_list
