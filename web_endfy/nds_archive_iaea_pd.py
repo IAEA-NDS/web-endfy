14e753cb09567341e9956236d9413c1dad7aae54
gschnabel <40870991+gschnabel@users.noreply.github.com>
2022-12-27 19:33:06 +0100

    refactor nds_archive_iaea_pd.py

--- web_endfy/nds_archive_iaea_pd.py
+++ web_endfy/nds_archive_iaea_pd.py
@@ -1,36 +1,30 @@
 from .endf_archive_downloader import EndfArchiveDownloader
 
 
-def create_iaea_pd1999_library_list(cache_dir, trafo_cache_ext):
-    iaea_pd1999_list = []
-    for sublib in ('g',):
-        iaea_pd1999_list.append(
-            EndfArchiveDownloader(
-                liburl=f'IAEA-PD-1999/{sublib}/',
-                libspec=f'iaea_pd1999_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return iaea_pd1999_list
-
-
-def create_iaea_pd2019_library_list(cache_dir, trafo_cache_ext):
-    iaea_pd2019_list = []
-    for sublib in ('g',):
-        iaea_pd2019_list.append(
-            EndfArchiveDownloader(
-                liburl=f'IAEA-PD-2019/{sublib}/',
-                libspec=f'iaea_pd2019_{sublib}',
-                cache_dir=cache_dir,
-                trafo_cache_ext=trafo_cache_ext
-            )
-        )
-    return iaea_pd2019_list
+iaea_pd_libraries_dict = {
+    'iaea_pd1999': {
+        'liburl': 'IAEA-PD-1999',
+        'sublibs': ('g',)
+    },
+    'iaea_pd2019': {
+        'liburl': 'IAEA-PD-2019',
+        'sublibs': ('g',)
+    }
+}
 
 
 def create_iaea_pd_library_list(cache_dir=None, trafo_cache_ext=None):
     iaea_pd_list = []
-    iaea_pd_list.extend(create_iaea_pd1999_library_list(cache_dir, trafo_cache_ext))
-    iaea_pd_list.extend(create_iaea_pd2019_library_list(cache_dir, trafo_cache_ext))
+    for curlibname, curlib in iaea_pd_libraries_dict.items():
+        for cursublib in curlib['sublibs']:
+            liburl = curlib['liburl'] + '/' + cursublib + '/'
+            libspec = curlibname + '_' + cursublib
+            iaea_pd_list.append(
+                EndfArchiveDownloader(
+                    liburl=liburl,
+                    libspec=libspec,
+                    cache_dir=cache_dir,
+                    trafo_cache_ext=trafo_cache_ext
+                )
+            )
     return iaea_pd_list
